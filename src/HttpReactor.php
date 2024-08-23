061b4dae112fd6204895a76fe068dc56178cb2d7
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-08-23 11:38:23 +0300

    + WebServerEngine

---
+++ src/HttpReactor.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types=1);
+
+namespace IfCastle\AmphpWebServer;
+
+use Amp\Http\HttpStatus;
+use Amp\Http\Server\DefaultErrorHandler;
+use Amp\Http\Server\Driver\SocketClientFactory;
+use Amp\Http\Server\RequestHandler\ClosureRequestHandler;
+use Amp\Http\Server\Response;
+use Amp\Http\Server\SocketHttpServer;
+use Amp\Socket\BindContext;
+use IfCastle\AmpPool\Worker\WorkerEntryPointInterface;
+use IfCastle\AmpPool\Worker\WorkerInterface;
+
+final class HttpReactor             implements WorkerEntryPointInterface
+{
+    private ?\WeakReference $worker = null;
+    
+    public function initialize(WorkerInterface $worker): void
+    {
+        $this->worker               = \WeakReference::create($worker);
+    }
+    
+    public function run(): void
+    {
+        $worker                     = $this->worker->get();
+        
+        if ($worker === null) {
+            return;
+        }
+        
+        $socketFactory              = $worker->getWorkerGroup()->getSocketStrategy()->getServerSocketFactory();
+        $clientFactory              = new SocketClientFactory($worker->getLogger());
+        $httpServer                 = new SocketHttpServer($worker->getLogger(), $socketFactory, $clientFactory);
+        
+        // 2. Expose the server to the network
+        $httpServer->expose('127.0.0.1:9095', (new BindContext())->withTcpNoDelay());
+        
+        // 3. Handle incoming connections and start the server
+        $httpServer->start(
+            new ClosureRequestHandler(static function () use ($worker): Response {
+                
+                return new Response(
+                    HttpStatus::OK,
+                    [
+                        'content-type' => 'text/plain; charset=utf-8',
+                    ],
+                    'Hello, World! From worker id: '.$worker->getWorkerId()
+                    .' and group id: '.$worker->getWorkerGroupId()
+                );
+            }),
+            new DefaultErrorHandler(),
+        );
+        
+        // 4. Await termination of the worker
+        $worker->awaitTermination();
+        
+        // 5. Stop the HTTP server
+        $httpServer->stop();
+    }
+}
--- src/WebServerEngine.php
+++ src/WebServerEngine.php
@@ -4,16 +4,163 @@ declare(strict_types=1);
 namespace IfCastle\AmphpWebServer;
 
 use IfCastle\AmpPool\WorkerPool;
+use IfCastle\Application\WorkerPool\WorkerTypeEnum;
+use IfCastle\AmpPool\WorkerTypeEnum as WorkerPoolTypeEnum;
+use IfCastle\Application\WorkerPool\WorkerGroup;
+use IfCastle\Application\WorkerPool\WorkerGroupInterface;
+use IfCastle\Application\WorkerPool\WorkerPoolBuilderInterface;
+use IfCastle\Application\WorkerPool\WorkerPoolInterface;
+use IfCastle\Application\WorkerPool\WorkerState;
+use IfCastle\Application\WorkerPool\WorkerStateInterface;
+use IfCastle\DI\ConfigInterface;
 use Psr\Log\LoggerInterface;
 
 class WebServerEngine               extends \IfCastle\Amphp\AmphpEngine
+                                    implements WorkerPoolBuilderInterface, WorkerPoolInterface
 {
-    public function __construct(private readonly LoggerInterface|null $logger = null) {}
+    protected array $workerGroups = [];
+    protected WorkerPool|null $workerPool = null;
+    
+    public function __construct(ConfigInterface $configuration, private readonly LoggerInterface|null $logger = null)
+    {
+        $this->applyConfiguration($configuration->findSection('server'));
+    }
     
     #[\Override]
     public function start(): void
     {
-        $workerPool                 = new WorkerPool(logger: $this->logger);
-        $workerPool->run();
+        if($this->workerPool !== null) {
+            return;
+        }
+        
+        $this->workerPool       = new WorkerPool(logger: $this->logger);
+        
+        foreach ($this->workerGroups as $group) {
+            $this->workerPool->describeGroup($group);
+        }
+        
+        $this->workerPool->run();
+    }
+    
+    #[\Override]
+    public function describeGroup(WorkerGroupInterface $group): void
+    {
+        $this->workerGroups[]       = $group;
+    }
+    
+    #[\Override]
+    public function getAllWorkerState(): array
+    {
+        $workerStates               = [];
+        
+        foreach ($this->workerPool->getWorkersStorage()->foreachWorkers() as $workerState) {
+            $workerStates[]         = new WorkerState(
+                workerId: $workerState->getWorkerId(),
+                groupId: $workerState->getGroupId(),
+                shouldBeStarted: $workerState->isShouldBeStarted(),
+                pid: $workerState->getPid()
+            );
+        }
+        
+        return $workerStates;
+    }
+    
+    #[\Override]
+    public function getWorkerState(int $workerId): WorkerStateInterface
+    {
+        $workerState                = $this->workerPool->getWorkersStorage()->getWorkerState($workerId);
+        
+        return new WorkerState(
+            workerId: $workerState->getWorkerId(),
+            groupId: $workerState->getGroupId(),
+            shouldBeStarted: $workerState->isShouldBeStarted(),
+            pid: $workerState->getPid()
+        );
+    }
+    
+    #[\Override]
+    public function getWorkerGroups(): array
+    {
+        $workerGroups               = [];
+        
+        foreach ($this->workerPool->getGroupsScheme() as $group) {
+            $workerGroups[]         = new WorkerGroup(
+                $group->getEntryPointClass(),
+                $this->workerPoolTypeToAppWorkerType($group->getWorkerType()),
+                $group->getMinWorkers(),
+                $group->getMaxWorkers(),
+                $group->getGroupName()
+            );
+        }
+        
+        return $workerGroups;
+    }
+    
+    #[\Override]
+    public function findGroup(int|string $groupIdOrName): WorkerGroupInterface|null
+    {
+        $group                     = $this->workerPool->findGroup($groupIdOrName);
+        
+        if($group === null) {
+            return null;
+        }
+        
+        return new WorkerGroup(
+            $group->getEntryPointClass(),
+            $this->workerPoolTypeToAppWorkerType($group->getWorkerType()),
+            $group->getMinWorkers(),
+            $group->getMaxWorkers(),
+            $group->getGroupName()
+        );
+    }
+    
+    #[\Override]
+    public function isWorkerRunning(int $workerId): bool
+    {
+        return $this->workerPool->isWorkerRunning($workerId);
+    }
+    
+    #[\Override]
+    public function restartWorker(int $workerId): bool
+    {
+        return $this->workerPool->restartWorker($workerId);
+    }
+    
+    private function workerPoolTypeToAppWorkerType(WorkerPoolTypeEnum $workerType): WorkerTypeEnum
+    {
+        return match ($workerType) {
+            WorkerPoolTypeEnum::REACTOR => WorkerTypeEnum::REACTOR,
+            WorkerPoolTypeEnum::JOB     => WorkerTypeEnum::JOB,
+            WorkerPoolTypeEnum::SERVICE => WorkerTypeEnum::SERVICE,
+        };
+    }
+    
+    private function applyConfiguration(array|null $config = null): void
+    {
+        if($config === null) {
+            return;
+        }
+        
+        $reactors                   = $config['reactors'] ?? 1;
+        $jobs                       = $config['jobs'] ?? 1;
+        
+        $reactors                   = (int)$reactors;
+        $jobs                       = (int)$jobs;
+        
+        $this->describeGroup(new WorkerGroup(
+                                 HttpReactor::class,
+                                 WorkerTypeEnum::REACTOR,
+                                 (int)$reactors,
+                                 0,
+                                 'Reactors'
+                             ));
+        
+        $this->describeGroup(new WorkerGroup(
+                                 JobWorker::class,
+                                 WorkerTypeEnum::JOB,
+                                 (int)$jobs,
+                                 0,
+                                 'Jobs'
+                             ));
     }
 }
