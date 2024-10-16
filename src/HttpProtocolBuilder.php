77dde86d0aa8a61bebf0e1e3a61f62efb883dbe8
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-10-16 10:10:07 +0300

    + HttpProtocolBuilder

--- src/HttpProtocolBuilder.php
+++ src/HttpProtocolBuilder.php
@@ -6,18 +6,16 @@ namespace IfCastle\AmphpWebServer;
 use IfCastle\Application\RequestEnvironment\RequestEnvironmentInterface;
 use Amp\Http\Server\Request;
 use IfCastle\Protocol\Http\HttpRequestInterface;
-use IfCastle\Protocol\RequestInterface;
 
 final class HttpProtocolBuilder
 {
     public function __invoke(RequestEnvironmentInterface $requestEnvironment): void
     {
-        if($requestEnvironment->hasDependency(RequestInterface::class)) {
+        if($requestEnvironment->hasDependency(HttpRequestInterface::class)) {
             return;
         }
         
         $originalRequest            = $requestEnvironment->originalRequest();
-        
         
         if(false === $originalRequest instanceof Request) {
             return;
