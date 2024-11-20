e94bbb1c723c56b67ec76e3408cae92b93d7dea9
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-11-20 11:04:56 +0200

    + add LoggerWithFilter

--- src/HttpReactorEngine.php
+++ src/HttpReactorEngine.php
@@ -11,12 +11,16 @@ use Amp\Http\Server\RequestHandler\ClosureRequestHandler;
 use Amp\Http\Server\Response;
 use Amp\Http\Server\SocketHttpServer;
 use Amp\Socket\BindContext;
+use IfCastle\AmpPool\Exceptions\FatalWorkerException;
 use IfCastle\AmpPool\Worker\WorkerInterface;
+use IfCastle\Application\Console\LoggerFilterByLevel;
 use IfCastle\Application\Environment\PublicEnvironmentInterface;
 use IfCastle\Application\Environment\SystemEnvironmentInterface;
 use IfCastle\Application\RequestEnvironment\RequestEnvironment;
 use IfCastle\Application\RequestEnvironment\RequestPlanInterface;
+use IfCastle\DI\ConfigInterface;
 use IfCastle\Exceptions\UnexpectedValueType;
+use Psr\Log\LogLevel;
 
 final class HttpReactorEngine extends \IfCastle\Amphp\AmphpEngine
 {
@@ -39,18 +43,31 @@ final class HttpReactorEngine extends \IfCastle\Amphp\AmphpEngine
             return;
         }
 
+        $systemEnvironment          = $this->systemEnvironment;
+        $config                     = $systemEnvironment->resolveDependency(ConfigInterface::class)->requireSection('server');
+
+        $host                       = $config['host'] ?? throw new FatalWorkerException('Config failed: Host is not defined');
+        $port                       = $config['port'] ?? '9095';
+        $isDebugMode                = $systemEnvironment->isDeveloperMode() && ($config['debugMode'] ?? false);
+
+        // Logger for the server engine
+        $serverLogger               = new LoggerFilterByLevel(
+            $worker->getLogger(), $isDebugMode ? LogLevel::DEBUG : LogLevel::ERROR
+        );
+
         $socketFactory              = $worker->getWorkerGroup()->getSocketStrategy()->getServerSocketFactory();
-        $clientFactory              = new SocketClientFactory($worker->getLogger());
-        $httpServer                 = new SocketHttpServer($worker->getLogger(), $socketFactory, $clientFactory);
+        $clientFactory              = new SocketClientFactory($serverLogger);
+        $httpServer                 = new SocketHttpServer($serverLogger, $socketFactory, $clientFactory);
 
         // 2. Expose the server to the network
-        $httpServer->expose('127.0.0.1:9095', (new BindContext())->withTcpNoDelay());
+        $httpServer->expose($host . ':' . $port, (new BindContext())->withTcpNoDelay());
 
-        $requestPlan                = $this->systemEnvironment->resolveDependency(RequestPlanInterface::class);
-        $systemEnvironment          = $this->systemEnvironment;
+        $requestPlan                = $systemEnvironment->resolveDependency(RequestPlanInterface::class);
         $publicEnvironment          = $systemEnvironment->findDependency(PublicEnvironmentInterface::class);
         $environment                = $publicEnvironment ?? $systemEnvironment;
 
+        $worker->getLogger()->info('HTTP server should be started on http://' . $host . ':' . $port);
+        
         // 3. Handle incoming connections and start the server
         $httpServer->start(
             new ClosureRequestHandler(static function (Request $request) use ($requestPlan, $environment): Response {
